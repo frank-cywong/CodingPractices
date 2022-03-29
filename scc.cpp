686ac6c6ca7528fb9f1345ce03785a6579f40f67
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2022-03-29 12:59:36 -0400

    Finish strongly connected components, kosajaru's algorithm

--- scc.cpp
+++ scc.cpp
@@ -3,26 +3,26 @@
 
 using namespace std;
 
-struct scc{
+struct SCC{
 	vector<vector<int>> adjlist; // if positive represents out, if negative represents in
 	vector<vector<int>> edgelist;
 	vector<vector<int>> sccgroups; // Vector of vectors, each subvector is a strongly connected component, with elements in vector being indicies
 	vector<bool> visited;
 	vector<int> finished;
 	vector<int> componentof;
-	scc(int N){
+	SCC(int N){
 		adjlist = vector<vector<int>>(N);
-		sccgroups = vector<vector<int>>;
+		sccgroups = vector<vector<int>>();
 	}
 	void addedge(int a, int b){ // adds directed edge from node a to b, where 0 <= a,b < N
 		adjlist[a].push_back(b);
-		adjlist[b].push_back(-a);
-		vector<int> vi = vector(2);
+		adjlist[b].push_back((-1*a)-1);
+		vector<int> vi = vector(2, 0);
 		vi[0] = a;
 		vi[1] = b;
 		edgelist.push_back(vi);
 	}
-	private void dfsone(int node){
+	private: void dfsone(int node){
 		if(visited[node]){
 			return;
 		}
@@ -35,7 +35,21 @@ struct scc{
 		}
 		finished.push_back(node);
 	}
-	void compscc(){
+	private: void dfstwo(int node, int grouptoaddto){
+		if(componentof[node] != -1 || visited[node]){
+			return;
+		}
+		visited[node] = true;
+		sccgroups[grouptoaddto].push_back(node);
+		componentof[node] = grouptoaddto;
+		for(auto it = adjlist[node].begin(); it != adjlist[node].end(); it++){
+			if(*it >= 0){
+				continue;
+			}
+			dfstwo((*it)*(-1)-1, grouptoaddto);
+		}
+	}
+	public: void compscc(){
 		visited = vector<bool>(adjlist.size(), false);
 		finished = vector<int>(adjlist.size(), false);
 		for(int i = 0; i < adjlist.size(); i++){
@@ -43,16 +57,35 @@ struct scc{
 		}
 		componentof = vector<int>(adjlist.size(), -1);
 		while(finished.size() != 0){
-			int curnode = finished.pop_back();
-			if(componentof[curnode] == -1){
+			int curnode = finished.back();
+			finished.pop_back();
+			if(componentof[curnode] != -1){
 				continue;
 			}
 			visited = vector<bool>(adjlist.size(), false);
-			dfstwo(curnode);
+			sccgroups.push_back(vector<int>());
+			dfstwo(curnode, sccgroups.size() - 1);
 		}
 	}
 };
 
 int main(){
+	int N; // node count
+	int E; // edge count
+	cin >> N >> E;
+	SCC graph(N);
+	for(int i = 0; i < E; i++){
+		int tempa, tempb;
+		cin >> tempa >> tempb;
+		graph.addedge(tempa, tempb);
+	}
+	graph.compscc();
+	for(auto it = graph.sccgroups.begin(); it != graph.sccgroups.end(); it++){
+		cout << "Strongly connected component: ";
+		for(auto it2 = it->begin(); it2 != it->end(); it2++){
+			cout << *it2 << " ";
+		}
+		cout << endl;
+	}
 	return 0;
 }
